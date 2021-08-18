9d801f77987ed44d89ff3aa24d104d95a99a42d1
sahil8889 <54908945+sahil8889@users.noreply.github.com>
2021-08-18 09:45:05 +0530

    tut01.py
    submission of tut01

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,1 +1,27 @@
-print("Hello World!...")
+import os
+os.system("cls")
+def meraki_helper(n):
+    """This will detect meraki numner"""
+    mylist= []
+    m=n
+    count=0
+    while n>0:
+        num=n%10
+        mylist.append(num)
+        n=n//10
+    for i in range(len(mylist)-1):
+        if(abs(mylist[i] - mylist[i+1]) != 1 ):
+            print("NO," , m, "is not a meraki number")
+            count=1
+            return 0
+    if(count==0):
+        print("YES," , m, "is a meraki number")
+
+    
+input = [12, 14, 56, 78, 98, 54, 678, 134, 789, 0, 7, 5, 123, 45, 76345, 987654321]
+t=0
+for i in range(len(input)):
+    if ( meraki_helper(input[i]) == 0 ):    
+        t=t+1
+print("The input list contains ", len(input)-t ,"meraki numbers and ", t ,"non meraki numbers.")  
+  
