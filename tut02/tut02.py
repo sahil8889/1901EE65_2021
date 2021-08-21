ffda7a65e43f46e9da013ef07f0c86ae750a9391
sahil8889 <54908945+sahil8889@users.noreply.github.com>
2021-08-21 08:59:40 +0530

    tut02.py
    submission of tut02

---
+++ tut02/tut02.py
@@ -0,0 +1,28 @@
+def get_memory_score(input_nums):
+    b=[]
+    t=0
+    for i in range(len(input_nums)):
+        if input_nums[i] in b:
+            t=t+1
+        elif(len(b)>4):
+            b.pop(0)
+            b.append(input_nums[i])
+        else:
+            b.append(input_nums[i])    
+    return t
+        
+
+input_nums = [3, 4, 1, 6, 3, 3, 9, 0, 0, 0]
+mylist=[]
+count=0
+for i in range(len(input_nums)):
+        if(str(input_nums[i]).isdigit()):
+            continue
+        else:
+            count=1
+            mylist.append(input_nums[i])
+if(count==1):    
+    print("Please enter a valid input list.")
+    print("Invalid inputs detected : ",mylist)
+else:
+    print("Score :", get_memory_score(input_nums))
