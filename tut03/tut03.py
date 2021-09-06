1786c426ac2a49a0fba6e6715c16faa933286014
sahil8889 <54908945+sahil8889@users.noreply.github.com>
2021-09-06 22:59:39 +0530

    tut3

--- tut03/tut03.py
+++ tut03/tut03.py
@@ -1,6 +1,56 @@
+import os
+
+
 def output_by_subject():
-    return
+    os.mkdir('output_by_subject')
+    file = "regtable_old.csv"    
+    with open(file, "r") as f:
+        for line in f:
+            column = line.split(',')
+            subno=column[3]
+            del column[4:8]
+            del column[2]
+            if (column[3] =="subno"):
+                continue
+            
+            path1 = 'output_by_subject/' + subno + '.csv'
+            try: 
+                with open(path1):
+                    with open(path1, "a") as f1:
+                        column=",".join(column)
+                        f1.write(column)
+
+            except IOError:
+                with open(path1, "w") as f1:
+                        f1.write("rollno,register_sem,subno,sub_type\n")
+                        column=",".join(column)
+                        f1.write(column)
+        return
 
 def output_individual_roll():
-    return
+    os.mkdir('output_individual_roll')
+    file = "regtable_old.csv"
+    with open(file, "r") as f:
+        for line in f:
+            column = line.split(',')
+            rollno=column[0]
+            del column[4:8]
+            del column[2]
+            if (column[0] =="rollno"):  
+                continue
 
+            path2 = 'output_individual_roll/' + rollno + '.csv'
+            try: 
+                with open(path2):
+                    with open(path2, "a") as f1:
+                        column=",".join(column)
+                        f1.write(column)
+            except IOError:
+                with open(path2, "w") as f1:
+                        f1.write("rollno,register_sem,subno,sub_type\n")
+                        column=",".join(column)
+                        f1.write(column)
+        return
+
+output_by_subject()
+output_individual_roll()
